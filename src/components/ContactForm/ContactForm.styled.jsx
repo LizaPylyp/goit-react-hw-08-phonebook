64e1c4c77f5664116533e300e0e1fa268d709fa9
Liza <lizkapylypchuk123456@gmail.com>
2023-10-02 20:29:14 +0300

    Update ContactForm.styled.jsx

--- src/components/ContactForm/ContactForm.styled.jsx
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -1,41 +1,53 @@
 import styled from '@emotion/styled';
 
-
 export const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    padding: 15px;
-    align-items: center;
-    background-color: #73bf20;
-    border: 2px solid grey;
-    border-radius: 3px;
-`;
-
-export const FormInput = styled.input`
-    width: 350px;
-    height: 40px;
-    border-radius: 3px;
-    font-size: 19px;
+  max-width: 350px;
+  margin: 0 auto;
+  border: 1px solid gray;
+  border-radius: 10px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: 1px 1px 5px gray;
+  background: #008080;
 `;
 
 export const FormLabel = styled.label`
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 20px;
-    font-size: 25px;
-    margin-bottom: 13px;
+  font-size: 28px;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 15px;
+  gap: 5px;
+`;
+
+export const FormInput = styled.input`
+  max-width: 300px;
+ 
+  height: 30px;
+  border-radius: 8px;
+  font-size: 20px;
+  padding: 10px;
 `;
 
 export const FormButton = styled.button`
-    margin: 0 auto;
-    padding: 7px 10px;
-    border-radius: 6px;
-    border: 1px solid red;
-    color:blue;
-    background-color: grey;
-    cursor: pointer;
-    font-size: 18px;
+  margin: 0 auto;
+  border-radius: 8px;
+  padding: 5px 10px;
+  background: transparent;
+  color: white;
 
+  text-shadow: 5px 5px 10px black;
+  font-size: 18px;
+
+  :hover,
+  :focus {
+    cursor: pointer;
+    background: #008080;
+  }
 `;
+
+
+
 
 
