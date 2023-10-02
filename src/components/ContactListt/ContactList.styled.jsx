96c46973b1812e9fd3729b6061e8d68ab9049044
Liza <lizkapylypchuk123456@gmail.com>
2023-10-02 20:29:18 +0300

    Update ContactList.styled.jsx

--- src/components/ContactListt/ContactList.styled.jsx
+++ src/components/ContactListt/ContactList.styled.jsx
@@ -1,34 +1,73 @@
 import styled from '@emotion/styled';
+import { BiSolidTrash } from 'react-icons/bi';
 
 export const ContactListStyled = styled.ul`
-    padding-left: 0;
-`;
+  padding-left: 0;
+  max-width: 450px;
+  margin: 0 auto;
 
-export const ButtonDelete = styled.button`
-    border-radius: 4px;
-    height: 33px;
-    padding: 0 4px;
-    background: #c274af;
-    color: white;
-    font-size: 16px;
-`;
-
-export const ContactData = styled.p`
-    display: flex;
-    flex-direction: column;
-    gap: 8px;
-    margin: 0;
+  @media screen and (min-width: 1048px) {
+    width: 450px;
+  }
 `;
 
 export const ContactItem = styled.li`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    font-size: 24px;
-    border-radius: 5px;
-    background: #c274af;
-    box-shadow: 1px 1px 5px #c0c0c0;
-    padding: 10px 20px;
-    margin-bottom: 15px;
-    border: 1px solid #c0c0c0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 26px;
+  border: 1px solid gray;
+  border-radius: 10px;
+  box-shadow: 1px 1px 5px gray;
+  padding: 10px 20px;
+  margin-bottom: 10px;
+  background: linear-gradient(180deg, #b5b5b5, #599caa, #008080);
+`;
+
+export const ContactData = styled.p`
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const Span = styled.span`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+export const ButtonDelete = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
+  border-radius: 8px;
+  height: 35px;
+  padding: 0 5px;
+  background: #008080;
+  color: white;
+
+  text-shadow: 5px 5px 10px black;
+  font-size: 18px;
+
+  :hover,
+  :focus {
+    cursor: pointer;
+    background: #2a7182;
+  }
+`;
+
+export const DeleteSpan = styled.span`
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const ButtonIcon = styled(BiSolidTrash)`
+  fill: currentColor;
+`;
+
+export const Text = styled.li`
+  display: block;
+  font-size: 28px;
 `;
