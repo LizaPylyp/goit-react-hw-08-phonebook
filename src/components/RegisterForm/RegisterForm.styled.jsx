e0626eb929729b3e492c004ddb064aabcf25bf6d
Liza <lizkapylypchuk123456@gmail.com>
2023-10-02 20:30:11 +0300

    Create RegisterForm.styled.jsx

---
+++ src/components/RegisterForm/RegisterForm.styled.jsx
@@ -0,0 +1,48 @@
+import styled from '@emotion/styled';
+
+export const Form = styled.form`
+  width: 300px;
+  margin: 0 auto;
+  padding: 10px;
+  border: 1px solid gray;
+  border-radius: 10px;
+  padding: 20px;
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
+  box-shadow: 1px 1px 5px gray;
+  background: #008080;
+`;
+
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  justify-content: space-between;
+  font-size: 18px;
+`;
+
+export const Input = styled.input`
+  max-width: 300px;
+  height: 30px;
+  border-radius: 8px;
+  font-size: 16px;
+  padding: 10px;
+`;
+
+export const Button = styled.button`
+  margin: 0 auto;
+  border-radius: 8px;
+  padding: 5px 10px;
+  background: #008080;
+  color: white;
+  font-size: 18px;
+
+  :hover,
+  :focus {
+    cursor: pointer;
+    background: #008080;
+  }
+`;
