6b38caddcba8db45591dd1b20052b06736f2beb4
Liza <lizkapylypchuk123456@gmail.com>
2023-10-02 17:42:33 +0300

    Create auth-selectors.js

---
+++ src/redux/auth/auth-selectors.js
@@ -0,0 +1,4 @@
+export const getIsLoggedIn = state => state.auth.isLoggedIn;
+export const getError = state => state.auth.error;
+export const getUser = state => state.auth.user;
+export const getIsRefreshing = state => state.auth.isRefreshing;
